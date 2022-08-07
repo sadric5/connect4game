faad131ed9b1792b718680f80eddb595d88907c3
sadric5 <sadricgamer@gmail.com>
2022-08-06 20:22:14 -0600

    Deployment ready

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 
 import websockets
 import secrets
+import os 
+import signal
 
 from connect4 import PLAYER1, PLAYER2, Connect4
 
@@ -148,8 +150,14 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future() # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 if __name__ == '__main__':
     asyncio.run(main())
